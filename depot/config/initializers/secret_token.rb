6f87784a58dc942678b430b1a9cfff915324f268
Isaac Hatton <isaac.hatton@brightonagency.com>
2011-06-02 10:57:12 -0500

    seeded products and made them look nice

--- depot/config/initializers/secret_token.rb
+++ depot/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = 'f8cccc1dbd8b78336d615ddb7a90084abbfbe403f2162f8dc3afbc3d6cc6d8bf88200d2e1b3159485fa90089d073db01e335d31a6ed4a56eff599bb40a5ca784'
+Depot::Application.config.secret_token = 'daabb44ff778852c8205ae9a78141ec70e988e352525d86f65fbe8bca6e88ef571b9a3fca32b73bbd34d1ccbd2228513a337348ea434c07df0d89a401da6af1d'
