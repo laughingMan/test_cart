54c0d222a4d27b6d41f7bde958e6b0c443d95764
Isaac <isaac.hatton@gmail.com>
2011-06-04 15:30:45 -0500

    Newly created rails app - first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'e7c46ce4890e47e40adcd0d33fc4d5a718194701f8cc82bf2e8e592544ee3269996091f42eee3c2148d03e70b1fa9a8954785e5b6ca40d6c2b5e4fc2bcbe6678'
